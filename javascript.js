281e87a6893b909fceeb8779e8af1f4865b908f4
Cavminer <Cavminer@users.noreply.github.com>
2016-06-10 22:37:56 -0400

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,9 @@
+var defintion = text;
+var query = prompt("What word do you want to research");
+
+switch {
+case Cavender: 
+text = "A highschool senior aspiring to be a computer/electric engineer";
+break;
+};
+window.open(text);
