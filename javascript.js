91037cf6b96f4a0ce0ceba3f858052256f67db70
Cavminer <Cavminer@users.noreply.github.com>
2016-06-12 09:51:30 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
-var defintion = text;
+var text;
 var query = prompt("What word do you want to research");
 
-switch {
-case Cavender: 
+switch (query) {
+case "Cavender": 
 text = "A highschool senior aspiring to be a computer/electric engineer";
 break;
-};
-window.open(text);
+}
+window.alert(text);
